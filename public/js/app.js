acdd4af51e40ba98e2a29c54c9d2c623ddbd0ffb
Marcel Degas <marcel.degas@gmail.com>
2016-06-27 13:55:58 -0700

    added autocomplete method

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,19 @@
+var formattedOriginLtLg,
+formattedDestinLtLg,
+directionsDisplay,
+map,
+options,
+address,
+origin,
+destination
+
 $('document').ready(function() {
+	initAutocomplete();
 
 })
+
+
+function initAutocomplete() {
+     autocompleteStart = new google.maps.places.Autocomplete((document.getElementById('start')), options);
+     autocompleteEnd = new google.maps.places.Autocomplete((document.getElementById('end')), options);
+}
